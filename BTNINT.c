c8b6ce4a319ca9911142343892417c3cc88c3aad
NotaScout <schoonwilliam@gmail.com>
2024-02-14 21:43:23 -0600

    -Put LED toggles on TIM6  -TODO:
    figure out wtf the TIM6 frequency is
    
    like: What is it???
    
    
    /// ?????? //

--- BTNINT.c
+++ BTNINT.c
@@ -12,6 +12,13 @@ int count=5;
 // no idea if we have a usable 4th LED, but the 3 that are usable appear to be R,G,&B ones...
 #define SWITCH 0 //PortA Pin0
 
+void LED_Toggle(int LED);
+void Init_LED(void);
+void Init_Switch(void);
+void Init_Timer6(uint32_t  PSC_num, uint32_t  ARR_num);
+
+
+// end prototypes
 
 void Init_LED(void){
 	RCC->AHB1ENR|=RCC_AHB1ENR_GPIOBEN;
@@ -44,6 +51,45 @@ void Init_Switch(void){
 		NVIC_EnableIRQ(EXTI0_IRQn);
 }
 
+void Init_Timer6(uint32_t  PSC_num, uint32_t  ARR_num)
+{
+	/* enables clock*/
+	RCC -> APB1ENR |= (RCC_APB1ENR_TIM6EN);
+	TIM6 ->CNT =0;
+	TIM6 ->PSC =PSC_num;
+	TIM6 ->ARR =ARR_num;
+	TIM6 ->DIER |= 1<<0; //enable UIE for interrupt
+	// starts the counter
+	TIM6 ->CR1 |= 1<<0;//bit 0 of CR1 is count enable
+	NVIC_SetPriority(TIM6_DAC_IRQn,0); //sets priority, if all are set to 0, than all share the same priority
+	NVIC_ClearPendingIRQ(TIM6_DAC_IRQn); //clears pending
+	NVIC_EnableIRQ(TIM6_DAC_IRQn); //enables
+	
+	__enable_irq();
+	
+}
+
+void TIM6_DAC_IRQHandler(void)
+{
+	NVIC_ClearPendingIRQ(TIM6_DAC_IRQn);
+	if(TIM6->SR & TIM_SR_UIF){ // if UIF flag is set -- Just in case
+		TIM6->SR &= ~TIM_SR_UIF; // clear UIF flag
+	}
+	LED_Toggle(LED_BLUE);
+	LED_Toggle(LED_GREEN);
+	LED_Toggle(LED_RED);
+
+}
+
+void LED_Toggle(int LED)
+{
+GPIOB->ODR ^= (1u<<LED);
+
+}
+
+
+/*
+
 void EXTI0_IRQHandler(void){	
 	//Clear the EXTI pending bits
 	EXTI->PR|=(1<<SWITCH);
@@ -51,7 +97,7 @@ void EXTI0_IRQHandler(void){
 	
 	GPIOB->BSRR = ((1<<(LED_ORANGE+16))|(1<<(LED_GREEN))|(1<<(LED_RED))|(1<<(LED_BLUE+16)));
 }
-
+*/
 
 
 int main(void)
@@ -59,12 +105,25 @@ int main(void)
 	int j;
 	Init_LED();
 	Init_Switch();
+	Init_Timer6(9999, 999); // TIM 6 has 16-bit resolution
+	
+	
+	/*
 	while(1)
 		{
 			GPIOB->BSRR = ((1<<(LED_ORANGE+16))|(1<<(LED_GREEN+16))|(1<<(LED_RED+16))|(1<<(LED_BLUE+16)));
 			GPIOB->BSRR = ((1<<(LED_ORANGE))|(1<<(LED_GREEN))|(1<<(LED_RED)) |(1<<(LED_BLUE)));
-			for(j=0; j<500000;j++){};
+			for(j=0; j<500000;j++){
+			int m = 0;
+			}
 			GPIOB->BSRR = ((1<<(LED_ORANGE+16))|(1<<(LED_GREEN+16))|(1<<(LED_RED+16))|(1<<(LED_BLUE+16)));
-			for(j=0; j<500000;j++){};
+			for(j=0; j<500000;j++){}
 		}
+	*/
+	while(1);
 }
+
+
+
+
+
