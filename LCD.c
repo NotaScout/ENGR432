52da99364fb679b48295921a57a523ec0804c182
Schoon <scho3988@STTHOMAS.EDU>
2024-02-29 15:15:27 -0600

    - Add inits to main

--- LCD.c
+++ LCD.c
@@ -57,3 +57,50 @@ void LCD_port_init(){
 
 }
 
+void LCD_init(){
+// STEP 1: Wait for 100ms for power-on-reset to take effect
+	int i;
+	for(i=0;i<=200;i++){}
+		
+// STEP 2: Set RS pin LOW to send instructions
+	GPIOD -> ODR &= ~(1u<<RS); // fix // odr 7 =  // ands the 7th odr pin with 0
+		
+// STEP 3a-3d: Set 4-bit mode (takes a total of 4 steps)
+		LCD_sendInstr(3); // 0010 0000 // sets 4-bit mode // 
+		//BF_check();
+		LCD_sendInstr(3); // home return
+		//BF_check();
+		LCD_sendInstr(3); // entry mode set (increment by 1)
+		//BF_check();
+		LCD_sendInstr(2); // turn on display
+		//BF_check();
+
+
+// STEP 4: Set 2 line display -- treats 16 char as 2 lines
+//			001DL NF** (DL 0: 4bits; N= 1: 2 lines; F=0 : 5x8 display
+	LCD_sendInstr(2); // we already did this is step 3a // 0010 0100
+	//BF_check();
+// STEP 5: Set DISPLAY to OFF
+	LCD_sendInstr(8);
+	LCD_sendInstr(0);
+	LCD_sendInstr(8);
+	LCD_sendInstr(0);
+	LCD_sendInstr(1);
+	LCD_sendInstr(0);
+	LCD_sendInstr(6);
+	LCD_sendInstr(0);
+	LCD_sendInstr(15);
+	LCD_sendInstr(0);
+	LCD_sendInstr(2);
+
+		
+		
+		
+}
+
+
+
+
+
+
+
