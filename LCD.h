6bb2e2ddd0cabb9bb72b298758c6155716b06441
Schoon <scho3988@STTHOMAS.EDU>
2024-04-09 14:26:58 -0500

    -Init commit

---
+++ LCD.h
@@ -0,0 +1,48 @@
+// PINSSSS
+
+// RW PINS
+// GPIOE
+#define RS 5
+
+// GPIOE
+#define RW 6
+
+// GPIOE
+#define EN 3
+
+// // DATA
+
+
+// GPIOF
+#define DB4 8
+
+// GPIOF
+#define DB5 7
+
+// GPIOF
+#define DB6 9
+
+// GPIOG
+#define DB7 1
+
+void LCD_PIN_clear(int PINNO);
+void LCD_PIN_set(int PINNO);
+void LCD_BF_check(void);
+void LCD_PIN_MODE(int PINNO, unsigned int MODE);
+
+void LCD_setup(void);
+void LCD_port_init(void);
+void LCD_init(void);
+
+void LCD_sendInstr(unsigned char Instruction);
+void LCD_sendData(unsigned char data);
+void LCD_clear(void);
+
+void LCD_send_String(char inputstring[]);
+void LCD_send_intString(int integer_number);
+void LCD_send_floatString(float float_number);
+
+void place_lcd_cursor(int lineno);
+void LCD_place_cursor(unsigned char lineno);//0x80 for 1st line and 0xC0 for 2nd line. Now sends a bit
+
+
--- MDK-ARM/HALTest/startup_stm32f429xx.d
+++
@@ -1,1 +0,0 @@
-haltest\startup_stm32f429xx.o: startup_stm32f429xx.s
